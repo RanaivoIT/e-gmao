23f169358e2fb06137e29d613b8bdef25912b556
ranaivo.it <ranaivo.it@gmail.com>
2022-09-11 23:00:43 +0300

    maj admin

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,9 +5,16 @@ namespace App\DataFixtures;
 use App\Entity\Administrateur;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
     public function load(ObjectManager $manager): void
     {
         $superadmin = new Administrateur();
@@ -19,7 +26,7 @@ class AppFixtures extends Fixture
                 ->setcontact("NA")
                 ->setEmail("NA")
                 ->setPassword($this->encoder->hashPassword($superadmin, "password"))
-                ->setAvatar("avatar.png");
+                ->setPicture("avatar.png");
         $manager->persist($superadmin);
         $manager->flush();
     }
