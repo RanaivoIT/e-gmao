100195393ff514fda6e5c0ed20569c257e5dc3f9
Micah <ranaivo.it@gmail.com>
2022-09-14 09:41:39 +0300

    Maj document controller

--- src/Controller/DocumentController.php
+++ src/Controller/DocumentController.php
@@ -5,8 +5,11 @@ namespace App\Controller;
 use App\Entity\Document;
 use App\Form\DocumentType;
 use App\Repository\DocumentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -16,7 +19,7 @@ class DocumentController extends AbstractController
     public function index(DocumentRepository $repo): Response
     {
         $documents = $repo->findAll();
-        return $this->render('colection/document/index.html.twig', [
+        return $this->render('document/index.html.twig', [
             'title' => 'Documents',
             'documents' => $documents,
         ]);
@@ -25,13 +28,28 @@ class DocumentController extends AbstractController
     #[IsGranted('ROLE_ADMINISTRATEUR')]
     public function add(Request $request, EntityManagerInterface $manager): Response
     {
-        $document = new Document();
-
-        $form = $this->createForm(DocumentType::class, $document);
+        $form = $this->createForm(DocumentType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            
+
+            $document = new Document();
+            $doc = $form->get('data')->getData();
+            if ($doc) {
+                $filename = "document" . mt_rand(1000000, 9999999) . "." . $doc->guessExtension();
+                try {
+                    $doc->move(
+                        $this->getParameter('pictures_directory'),
+                        $filename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+                $document->setData($filename);
+                $document->setName($form->get('name')->getData());
+                $document->setColection($form->get('colection')->getData());
+            }
+
             $manager->persist($document);
             $manager->flush();
 
@@ -45,31 +63,52 @@ class DocumentController extends AbstractController
             ]);
         }
 
-        return $this->render('colection/document/add.html.twig', [
+        return $this->render('document/add.html.twig', [
             'title' => 'Document - Add',
-            'document' => $document,
             'form' => $form->createView()
         ]);
     }
+
     #[Route('/documents/{id}', name: 'documents_show')]
     public function show(Document $document): Response
     {
         return $this->render('document/show.html.twig', [
             'title' => 'Documents - Show',
+            'url_doc'=> $this->getParameter('pictures_url') . $document->getData(),
             'document' => $document
         ]);
     }
+
+    #[Route('/documents/{id}/pdf', name: 'documents_pdf')]
+    public function pdf(Document $document): Response
+    {
+        return new BinaryFileResponse($this->getParameter('pictures_directory') . '/' . $document->getData());
+    }
+
     #[Route('/documents/{id}/edit', name: 'documents_edit')]
     #[IsGranted('ROLE_ADMINISTRATEUR')]
     public function edit(Document $document, Request $request, EntityManagerInterface $manager): Response
     {
-        $form = $this->createForm(DocumentType::class, $document);
+        $form = $this->createForm(DocumentType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             
-            $manager->persist($document);
-            $manager->flush();
+            $doc = $form->get('data')->getData();
+            if ($doc) {
+                $filename = "document" . mt_rand(1000000, 9999999) . "." . $doc->guessExtension();
+                try {
+                    $doc->move(
+                        $this->getParameter('pictures_directory'),
+                        $filename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+                $document->setData($filename);
+                $document->setName($form->get('name')->getData());
+                $document->setColection($form->get('colection')->getData());
+            }
 
             $this->addFlash(
                 'success',
